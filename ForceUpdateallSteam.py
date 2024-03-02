496b672ab64c629a369dafa2e413b27865b60093
N3ox1s <82593346+N3ox1s@users.noreply.github.com>
2024-03-02 18:04:20 +0100

    working

--- ForceUpdateallSteam.py
+++ ForceUpdateallSteam.py
@@ -1,29 +1,29 @@
+
 import os
 
-# Path to steamapps
-path = "D:/games/Steam/steamapps/"
-
+here = os.path.dirname(os.path.abspath(__file__))
 
 #path = input("Enter Path to steamapps:")
-if os.path.isfile('./path.txt') is False:
+if os.path.isfile(here + "\path.txt") is False:
     f = open("path.txt", "w")
     f.write(input("Enter Path to steamapps:"))
     f.close()
 
-f = open("path.txt", "r")
+f = open(here + "\path.txt", "r")
 path = f.read()
 f.close()
 
+print("searching in " + path)
 
-def replace():
+def run_replace():
     for filename in os.listdir(path):
         if filename.startswith("appmanifest_"):
-            with open(path + filename, "r") as file:
+            with open(path + "/" + filename, "r") as file:
                 content = file.read()
-                with open(path + filename, "w") as f:
+                with open(path + "/" + filename, "w") as f:
                     f.write(content.replace('\t"AutoUpdateBehavior"\t\t"0"\n', '\t"AutoUpdateBehavior"\t\t"2"\n'))
 
-
+run_replace()
 print("Done")
-print("Press Enter to continue ...")
+print("Press Enter to exit ...")
 input()
